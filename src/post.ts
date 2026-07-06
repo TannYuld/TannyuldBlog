cdb94fb9bb2aebc1e294292d6bf5169a8fb9c5ad
TannYuld <onuraltintas2005@gmail.com>
2026-07-06 11:24:59 +0200

    -gitignore changed -dist folder included

--- src/post.ts
+++ src/post.ts
@@ -1,20 +1,27 @@
 import { fetchDataIfIntegrityNotMatch, getBlogs, retrieveData, subscribeBlogPostsChanges, type BlogPost } from "./shared";
 
+let id: number;
+
 addEventListener("load", (_) => {
+    const params = new URLSearchParams(window.location.search);
+    const maybeId = params.get('id');
+    if (maybeId === null) {
+        window.location.replace("index.html");
+        return;
+    }
+    id = Number(maybeId);
+
     subscribeBlogPostsChanges(whenBlogPostsChange);
     retrieveData();
     fetchDataIfIntegrityNotMatch();
 })
 
 function whenBlogPostsChange() {
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get('id');
-    if (id === null || Number(id) > getBlogs().length) {
+    if (id > getBlogs().length) {
         window.location.replace("index.html");
         return;
     }
-
-    const selectedPost = getBlogs()[Number(id)] as BlogPost;
+    const selectedPost = getBlogs()[id] as BlogPost;
     (document.getElementById("post-title") as HTMLElement).textContent = selectedPost.title;
     const formattedDate = new Intl.DateTimeFormat('en-GB', {
         day: '2-digit',
